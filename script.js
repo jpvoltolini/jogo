eba582241892f90623102f1c85db367980c6675d
João Pedro Voltolini <jptheshogun@gmail.com>
2022-09-28 20:11:06 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    // canvas setup
+    const canvas = document.getElementById(canvas1);
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    canvas.height = 500;
+
+});
